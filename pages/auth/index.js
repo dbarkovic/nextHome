c66c3aa135f10352d72572f5221be1e4585805b3
Dinko Barković <dinko@neos.hr>
2021-03-07 19:07:15 +0100

    Init commit

---
+++ pages/auth/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const auzhIndexPage = () => (
+    <div>
+        <h1>The second auth page</h1>
+    </div>
+)
+
+export default auzhIndexPage;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const indexPage = () => (
+    <div>
+        <h1>The Main Root page</h1>
+    </div>
+)
+
+export default indexPage;
